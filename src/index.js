6309fde2bb7415397405e161a730c15c47a3c0b6
vishakha <vishakhaagrawal0508@gmail.com>
2026-09-18 11:56:02 +0530

    completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
+import cors from "cors"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -20,6 +21,11 @@ const app = express();
 
 app.use(express.json());
 app.use(rateLimiter);
+app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
 app.use((req,res,next)=>{
     console.log(`Req method is ${req.method} & req url is ${req.url}`);
     next()
